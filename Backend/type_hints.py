21e029bd29f3cec3d509570681e72e623d194e88
Kamila Ojeda <beau.ojeda@gmail.com>
2023-04-17 14:08:47 -0300

    Type hints.

---
+++ Backend/type_hints.py
@@ -0,0 +1,21 @@
+## Type Hints ##
+
+# Quiere decir que es de tipado dinámico
+string_variable = "String variable"
+print(string_variable)
+print(type(string_variable))
+
+# El tipo de ha cambiado en tiempo de ejecución
+string_variable = 5
+print(string_variable)
+print(type(string_variable))
+
+# Tipado débil
+my_typed_variable: int = 7
+my_typed_variable.
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+my_typed_variable= "My typed String variable"
+print(my_typed_variable)
+print(type(my_typed_variable))
