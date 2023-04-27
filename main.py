1131964cbff14a64ddcc108c9f943c2cbf7376dc
Kamila Ojeda <beau.ojeda@gmail.com>
2023-04-27 12:17:49 -0300

    Recursos Estáticos.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI # Importamos fastAPI
-from routers import products, users #Importamos el fichero de products y users DESDE LA CARPETA ROUTERS, que es donde están ubicados.
-
+from routers import products, users # Importamos el fichero de products y users DESDE LA CARPETA ROUTERS, que es donde están ubicados.
+from fastapi.staticfiles import StaticFiles # Importamos la clase para poder manipular los recursos estáticos.
 app = FastAPI() #Creamos un objeto app, por convención, que instancie la clase FastAPI()
 
 # ROUTERS
 ## Accedemos a la instancia app y operacion: include_router
 app.include_router(products.router)
 app.include_router(users.router)
+
+# RECURSOS ESTÁTICOS
+## Para exponer un recurso estático
+app.mount("/static", StaticFiles(directory="static"), name="static")
+## http://127.0.0.1:8000/static/images/backend.png
 
 # Creamos una función que se llame root
 # Con @app ya estamos accediendo al contexto de fastAPI
